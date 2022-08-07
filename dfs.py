cc5dad36d592446407bb7b08c823f14580db8a11
Majo Paulose <109265290+majopaul22@users.noreply.github.com>
2022-08-07 17:00:34 -0500

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,23 +1,24 @@
 # DFS (Depath first search)
-# run this in online python interpretor such as - https://www.programiz.com/python-programming/online-compiler/
+# run this in online python interpretor such as - 
+# https://www.programiz.com/python-programming/online-compiler/
 
 graph = {
-'5' : ['3','7'],
+  '5' : ['3','7'],
   '3' : ['2', '4'],
   '7' : ['8'],
   '2' : [],
   '4' : ['8'],
   '8' : []
-
 }
 
 visited = set()
+
 def dfs(visited, graph, node):
   if node not in visited:
+    print (node)
     visited.add(node)
-    print(node)
     for neighbour in graph[node]:
-      dfs(visited, graph, node)
+      dfs(visited, graph, neighbour)
 
 
 print("Follow Depth first search")
